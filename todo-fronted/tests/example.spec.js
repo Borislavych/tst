dfb02aceb17e3c7c1e3803335ba2f55f35c91a3b
Borislavych <a.ershov@life-stream.tv>
2023-04-02 16:58:18 +0400

    add and delete todo

--- todo-fronted/tests/example.spec.js
+++ todo-fronted/tests/example.spec.js
@@ -5,5 +5,13 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   await expect(page.getByTestId('title-home-page')).toHaveText('Todo App');
+  await page.getByTestId('add-title').type('123');
+  await page.getByTestId('add-description').type('456');
+  await page.getByTestId('button-add-todo').click();
+  //КАКОГО ХУЯ ОН С ПЕРВОГО РАЗА НЕ ПРОКЛИКИВАЕТСЯ БЛЯТЬ???
+  await page.getByTestId('button-add-todo').click();
+  await page.getByTestId('button-delete').click();
+  await expect(page.getByTestId('text-no-todo')).toHaveText('Sorry! No Todos Found.');
+
 });
 
